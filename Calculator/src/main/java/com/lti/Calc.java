06abd7a9c7e6b2244f69572136dcca5b6193105f
akhilakotha123 <kothaakhila123@gmail.com>
2020-12-21 12:43:47 +0530

    first commit

---
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -0,0 +1,13 @@
+package com.lti;
+
+public class Calc {
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sum="+add(10,20));
+	}
+
+}
